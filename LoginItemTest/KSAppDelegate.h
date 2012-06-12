c066fd9b6d3605655d0fce3b606669f52766fd95
Keith Smiley <keithbodesmiley@gmail.com>
2012-06-12 09:11:07 -0400

    Initial Commit

---
+++ LoginItemTest/KSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KSAppDelegate.h
+//  LoginItemTest
+//
+//  Created by Keith Smiley on 6/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ LoginItemTest/KSAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  KSAppDelegate.m
+//  LoginItemTest
+//
+//  Created by Keith Smiley on 6/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "KSAppDelegate.h"
+
+@implementation KSAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
